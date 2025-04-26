fa35479c0bf354336cd137017b8653ec841b2c4f
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-26 18:02:09 +0500

    update

--- db.js
+++ db.js
@@ -6,6 +6,6 @@ const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 //const sql = neon(`postgresql://neondb_owner:npg_jQzOurV9of0F@ep-withered-breeze-a4rh87h0-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require`)
 
-export const sql = neon(process.env.DATABASE_URL);
+export const sql = neon('postgresql://neondb_owner:npg_jQzOurV9of0F@ep-withered-breeze-a4rh87h0-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require');
 
 
