262cac93f9729454e0bd17c64bf81a94bcbc5749
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-04-26 15:04:35 +0500

    update

---
+++ db.js
@@ -0,0 +1,16 @@
+// db.js
+import neon from '@neondatabase/serverless'
+// Neon automatically handles pooling and connection reuse
+
+import { neon } from '@neondatabase/serverless';
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+//const sql = neon(`postgresql://neondb_owner:npg_jQzOurV9of0F@ep-withered-breeze-a4rh87h0-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require`)
+
+const sql = neon(process.env.DATABASE_URL);
+
+async function connectDB() {
+  return sql;
+}
+
+module.exports = { connectDB };
