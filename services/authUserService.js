b761447ad6ac75c0f537e553e5c2313495b77bed
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-05-31 00:09:27 +0500

    update

--- services/authUserService.js
+++ services/authUserService.js
@@ -35,6 +35,7 @@ export async function updateUserProfile(userId, { name, avatar }) {
     select: { id: true, name: true, avatar: true }, // safer return shape
   });
 
-  return updatedUser;
+  //return updatedUser;
+  res.json(updatedUser); 
 }
 
